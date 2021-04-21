ed09d690b2e8a7c52dfcf03919a5ace281e7b9f2
Colter Ulrich <cju5025@gmail.com>
2021-04-20 18:06:50 -0600

    adds iteration folder for loop testing

---
+++ iteration/iteration_test.go
@@ -0,0 +1,38 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("testing on single character input", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("testing on multiple character input", func(t *testing.T) {
+		got := Repeat("she", 3)
+		want := "shesheshe"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("testing on multiple character input with spaces", func(t *testing.T) {
+		got := Repeat("she wants ", 3)
+		want := "she wants she wants she wants "
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+// go test -bench=. is the command to run a benchmark test
