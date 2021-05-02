01abc2f8744b92b027833ff514f057316b3b4c1d
Colter Ulrich <cju5025@gmail.com>
2021-05-02 17:33:00 -0600

    adds test for slices to arrays folder

---
+++ arrays-and-slices/sum.go
@@ -0,0 +1,17 @@
+package main
+
+func SumArray(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
